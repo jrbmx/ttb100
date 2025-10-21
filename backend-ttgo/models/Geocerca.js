3dc4da387b8aa9fc53fb24e874a1e2f11408cad0
AK_111124-at-707515407085 <111124@horusur.corp>
2025-10-20 22:13:51 -0600

    Geocercas actualizado

--- backend-ttgo/models/Geocerca.js
+++ backend-ttgo/models/Geocerca.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const GeocercaSchema = new mongoose.Schema({
   cuidador: { type: mongoose.Schema.Types.ObjectId, ref: 'Cuidador', required: true },
-  paciente: { type: mongoose.Schema.Types.ObjectId, ref: 'Paciente', required: true }, // <- sin unique
+  paciente: { type: mongoose.Schema.Types.ObjectId, ref: 'Paciente', required: true }, // <-- sin unique
   coords: [{
     lat: { type: Number, required: true },
     lng: { type: Number, required: true }
