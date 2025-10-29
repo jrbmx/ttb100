b79cba668ab58b96ff7bea375a1cb9be451972d4
Alberto Vega <betovegamon@gmail.com>
2025-10-29 00:39:41 -0600

    Remove Counter model and update related logic
    Deleted the Counter model and removed all references to it in the Cuidador model and related routes. Updated cuidador and paciente registration and login logic to no longer use auto-incremented IDs. Improved data validation and added new endpoints for retrieving patient data in datos.js.

--- backend-ttgo/models/Cuidador.js
+++ backend-ttgo/models/Cuidador.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const CuidadorSchema = new mongoose.Schema({
-  id_cuidador: { type: Number, required: true, unique: true },
+  //id_cuidador: { type: Number, required: true, unique: true },
   nombre: String,
   apellidoP: String,
   apellidoM: String,
@@ -11,7 +11,7 @@ const CuidadorSchema = new mongoose.Schema({
   verificado: { type: Boolean, default: false },
 
   // 👇 que sea NUMBER, porque en pacientes guardas el autoincrementable (counter.seq)
-  id_paciente: { type: Number, default: null } 
+  //id_paciente: { type: Number, default: null } 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Cuidador', CuidadorSchema);
